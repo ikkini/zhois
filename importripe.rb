343a32633c843f37a55dd9cd68b31087b5c3c99b
ikkini <ikkini@gmail.com>
2014-05-22 13:07:33 +0200

    readd RIPE countries/netnames zadd, format change
    The functionality will add a sorted set of country names => net names
    and net names => country names.

--- importripe.rb
+++ importripe.rb
@@ -9,7 +9,8 @@ ripe.flushdb
 CSV.foreach('src/ripe.formatted.src', col_sep: '|') do |row|
   networkip = row[0].lstrip
   broadcastip = row[1]
-  iprangesize = NetAddr.range(networkip, broadcastip, Inclusive: true, Size: true)
+  iprangesize = NetAddr.range(networkip, broadcastip,
+                              Inclusive: true, Size: true)
   # skip any range above a /8
   if iprangesize < 16_777_216
     # split rows and convert to correct lower:UPPER format
@@ -17,8 +18,10 @@ CSV.foreach('src/ripe.formatted.src', col_sep: '|') do |row|
     v3 = row[3].split(':')
     v2 = v2[0] << ':' << v2[1].upcase
     v3 = v3[0] << ':' << v3[1].rstrip.upcase
-    ripe.zadd(networkip, [[iprangesize, broadcastip], [iprangesize, v2], [iprangesize, v3]])
-    #ripe.zadd(v2, iprangesize, v3)
-    #ripe.zadd(v3, iprangesize, v2)
+    ripe.zadd(networkip, [[iprangesize, broadcastip],
+                          [iprangesize, v2],
+                          [iprangesize, v3]])
+    ripe.zadd(v2, iprangesize, v3)
+    ripe.zadd(v3, iprangesize, v2)
   end
 end
