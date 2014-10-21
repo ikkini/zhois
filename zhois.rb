1aa3db64add1484db7232ebd33df12bfbfaaec88
ikkini <ikkini@gmail.com>
2014-10-21 22:11:50 +0200

    cleanup

--- zhois.rb
+++ zhois.rb
@@ -1,59 +1,61 @@
-# encoding: UTF-8
+# Encoding: utf-8
+require 'csv'
+require 'netaddr'
 require 'hiredis'
 require 'redis'
-require 'netaddr'
 
-# Connect to Redis databases.
-# 6 contains found IPs.
-# 7 contains zsets: k:first inetnum, v=last inetnum, score=networksize,
-#   v=netname, score=networksize, v=country, score=networksize.
-iplist = Redis.new(db: 6, driver: 'hiredis')
-ripe = Redis.new(db: 7, driver: 'hiredis')
-results = Redis.new(db: 1, driver: 'hiredis')
+# added longer timeout, otherwise flushdb results in TimeoutErrors
+geoip = Redis.new(db: 1, driver: 'hiredis', timeout: 30)
+#synip = Redis.new(db: 2, driver: 'hiredis', timeout: 30)
+rsyncip = Redis.new(db: 3, driver: 'hiredis', timeout: 30)
+#puts 'Flushing dbs'
+geoip.flushdb
+#synip.flushdb
+rsyncip.flushdb
+puts 'Adding network(int):broadcast(int):CC GeoIP values to index:geoip'
 
-# Flush the results database
-puts 'flush the results database'
-results.flushdb
-
-puts 'Iterate over iplist keys'
-iplist.keys.each do |ip|
-  netaddrlist = []
-  # RIPE DB 7 does not contain any subnet larger than a /8 and RIPE should
-  #    not have any CIDR larger than a /30.
-  30.downto(8) do |mask|
-    netaddr = NetAddr::CIDRv4.create("#{ip}/#{mask}")
-    # Use NetAddr to create pairs of network address and subnet size.
-    netaddrlist << [netaddr.first, netaddr.size, netaddr.last]
-  end
-  netaddrlist.each do |naddr|
-    # OPTIMIZE: How to make this cheaper?
-    # below assumes we know what is at a[1] (country, cause it starts
-    # with a c) and a[2] (netname)
-    a = ripe.zrangebyscore(naddr[0], naddr[1], naddr[1])
-    next unless a.size == 3
-    # Insert 4 record types per match: ip:, inetnum:, country:, and netname:.
-    # Each of these contains the others, everything sorted (zadd score) by
-    # iprangesize.
-    #
-    results.multi do
-      # it adds a fraction extra time to the parsing to assign these to
-      # readable values
-      inetnum = 'inetnum:' + naddr[0] + '-' + naddr[2]
-      country = a[1]
-      netname = a[2]
-      netrange = naddr[1]
-      results.zadd(inetnum, [[netrange, 'ip:' + ip], [netrange, country],
-                             [netrange, netname]])
-      results.zadd(country, [[netrange, 'ip:' + ip], [netrange, netname],
-                             [netrange, inetnum]])
-      results.zadd(netname, [[netrange, 'ip:' + ip], [netrange, country],
-                             [netrange, inetnum]])
-      results.zadd('ip:' + ip, [[netrange, netname], [netrange, country],
-                                [netrange, inetnum]])
-      results.sadd('index:ip', ip)
-      results.sadd('index:inetnum', inetnum)
-      results.sadd('index:country', country)
-      results.sadd('index:netname', netname)
+# use geoip/create.sh
+CSV.foreach('geoip/geoipmatrix.csv', col_sep: ',') do |row|
+  unless row[2].to_i == 0
+    if row[2].to_i - row[9].to_i > 1
+      # Add skipped range as an added row.
+      geoip.rpush('index:geoip', ((row[8].to_i - 1) +
+                                  (row[2].to_i - row[9].to_i)).to_s +
+                                  ':' + (row[2].to_i - 1).to_s + ':SKIP')
+      # Add actual row
+      geoip.rpush('index:geoip', row[2] + ':' + row[3] + ':' + row[4])
+    else
+      geoip.rpush('index:geoip', row[2] + ':' + row[3] + ':' + row[4])
     end
+  end
+end
+# sorted list, using sort -t . -n -k 1,1n -k 2,2n -k 3,3n -k 4,4n
+#puts 'Adding "sorted ip_to_i" found IPs responding to syn to index:synip'
+#CSV.foreach('src/synip') do |row|
+#  synip.rpush('index:synip', NetAddr.ip_to_i(row[0], Version: 4))
+#end
+puts 'Adding "sorted ip_to_i" found IPs with actual rsync responses to index:rsyncip'
+CSV.foreach('src/rsyncip') do |row|
+  rsyncip.rpush('index:rsyncip', NetAddr.ip_to_i(row[0], Version: 4))
+end
+
+
+# because of how we currently create the geoipmatrix, we need to pop the
+# first instance
+geoip.lpop('index:geoip')
+
+# see if rsyncip in country
+#   if yes store rsyncip + pop rsyncip + get next rsyncip
+#   if not, pop country and get next country
+until rsyncip.llen('index:rsyncip') == 0
+  ipaddress = rsyncip.lindex('index:rsyncip', 0)
+  country = geoip.lindex('index:geoip', 0).split(':')
+  if ipaddress.to_i <= country[1].to_i
+    foundip = rsyncip.lpop('index:rsyncip')
+    #puts "popping #{foundip}: found in #{country}"
+    rsyncip.rpush('res:' + country[2], NetAddr.i_to_ip(foundip.to_i, Version: 4))
+  else
+    geoip.lpop('index:geoip')
+    #puts "popping #{geoiprange}"
   end
 end
